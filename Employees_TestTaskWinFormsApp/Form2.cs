c2847cdc34afa27537a8df6ed6443570cc3b5873
FoxAlina <nikitinaalina2001@gmail.com>
2023-11-07 18:11:23 +0300

    InterfaceAdjustment

--- Employees_TestTaskWinFormsApp/Form2.cs
+++ Employees_TestTaskWinFormsApp/Form2.cs
@@ -22,10 +22,6 @@ namespace Employees_TestTaskWinFormsApp
             // данная строка кода позволяет загрузить данные в таблицу "EmployeesDataSet.AverageSalaryInPositionInfo". При необходимости она может быть перемещена или удалена.
             this.AverageSalaryInPositionInfoTableAdapter.Fill(this.EmployeesDataSet.AverageSalaryInPositionInfo);
 
-            //reportViewer1.LocalReport.ReportEmbeddedResource = "Employees_TestTaskWinFormsApp.Report1.rdlc";
-            //reportViewer1.LocalReport.DataSources.Add(
-            //    new Microsoft.Reporting.WinForms.ReportDataSource("DataSet1", EmployeesDataSet.Tables[0]));
-
             this.reportViewer1.RefreshReport();
         }
     }
